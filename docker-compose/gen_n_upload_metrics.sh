8d289c931605b24df96d265f4c302a0dfea043c6
Сергей Михайлов <s.mihaylov@tionix.ru>
2021-01-09 12:41:32 +0300

    Add srv_id

--- docker-compose/gen_n_upload_metrics.sh
+++ docker-compose/gen_n_upload_metrics.sh
@@ -4,11 +4,12 @@ VM_ADDR='http://localhost:8428'
 VM_LOGIN=kp_resort_meteo
 VM_PASSWORD=kp_resort_meteo
 CUR_DATE="$(TZ=UTC date '+%d-%m-%y_%H-%M-%S')"
+SRV_ID="$(( RANDOM % 999 ))"
 
 for PLACE in $PLACES; do
 	WIND_SPEED="$(( RANDOM % 30 )).$(( RANDOM % 10 ))"
 	WIND_DIRECTION="$(( RANDOM % 361 )).$(( RANDOM % 10 ))"
 	curl -s -u "${VM_LOGIN}:$VM_PASSWORD" \
-		-d "${CUR_DATE},${WIND_SPEED},${WIND_DIRECTION}" \
-		"http://localhost:8428/api/v1/import/csv?format=1:time:custom:02-01-06_15-04-05,2:metric:kp_meteo_ws_mean,3:metric:kp_meteo_wd_mean&extra_label=location=${PLACE}"
+		-d "${CUR_DATE},${SRV_ID},${WIND_SPEED},${WIND_DIRECTION}" \
+		"http://localhost:8428/api/v1/import/csv?format=1:time:custom:02-01-06_15-04-05,2:label:srv_id,3:metric:kp_meteo_ws_mean,4:metric:kp_meteo_wd_mean&extra_label=location=${PLACE}"
 done
